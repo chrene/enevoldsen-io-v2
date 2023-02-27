fd4447f3a7f60083f0e29797adfb9ddc6efa68da
chrene <chrene91@gmail.com>
2023-02-27 22:05:50 +0100

    updates

--- src/pages/freelance.tsx
+++ src/pages/freelance.tsx
@@ -10,12 +10,10 @@ export default function FreelancePage() {
   return (
     <Layout>
       <Head>
-        <title>
-          Christian - Freelance webudvikling til mindre projekter og startups.
-        </title>
+        <title>Freelance webudvikling til mindre projekter og startups.</title>
         <meta
           name="description"
-          content="Jeg tilbyder webudviklings- og designløsninger på mindre projekter til enkeltpersoner, startups og mindre virksomheder på freelancebasis"
+          content="Jeg kan hjælpe dig med at udvikle og designe din hjemmeside, uanset om du er en enkeltperson, en opstartsvirksomhed eller en mindre virksomhed. Som freelance webudvikler kan jeg tilbyde professionelle løsninger på mindre projekter. Kontakt mig for at finde ud af, hvordan jeg kan hjælpe dig."
         />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
@@ -24,35 +22,42 @@ export default function FreelancePage() {
           "Freelance webudvikling til mindre projekter og startups."
         }
         renderDescription={() =>
-          "Jeg har igennem min karriere arbejdet med mange forskellige projekter, både som freelancer og som ansat."
+          "Hej! Hvis du overvejer at oprette en hjemmeside, så er jeg her for at hjælpe. Jeg er en freelance webudvikler med erfaring i at skabe professionelle løsninger til enkeltpersoner, startups og mindre virksomheder. Lad mig hjælpe dig med at realisere dit online potentiale med en unik og skræddersyet hjemmeside. Kontakt mig for at få mere information om, hvordan jeg kan hjælpe dig med dit projekt."
         }
         titleClassName="lg:text-5xl lg:leading-tight"
         showScrollIndicator
       />
       <Services />
-      <ProfessionalProjects />
       <About
         renderTitle={() => (
-          <AboutTitle>Freelance for mindre projekter og startups.</AboutTitle>
+          <AboutTitle>
+            Professionel webudvikling og design til mindre projekter og startups
+          </AboutTitle>
         )}
         renderDescription={() => (
           <>
-            Jeg tilbyder webudviklings- og designløsninger på mindre projekter
-            til enkeltpersoner, startups og mindre virksomheder på
-            freelancebasis. <br />
-            <br />
-            Jeg kan hjælpe dig med at opbygge en unik og effektiv online
-            tilstedeværelse, uanset om det drejer sig om en hjemmeside, en
-            webshop eller en webbaseret applikation.
+            Jeg tilbyder webudviklings- og designløsninger på freelancebasis, og
+            jeg er klar til at hjælpe dig med dit projekt. Uanset om du er en
+            enkeltperson, en startup eller en mindre virksomhed, kan jeg hjælpe
+            dig med at opbygge en unik og effektiv online tilstedeværelse.
             <br />
             <br />
             Jeg tilbyder en række tjenester inden for webudvikling og design,
-            inklusiv responsiv webdesign, tilpasning af CMS (Content Management
+            inklusiv responsivt webdesign, tilpasning af CMS (Content Management
             System), integration af e-handelsløsninger og udvikling af
-            webapplikationer.
+            webapplikationer. Alt sammen for at sikre, at din hjemmeside,
+            webshop eller webbaserede applikation er tilpasset dine behov og
+            krav.
+            <br />
+            <br />
+            Lad mig hjælpe dig med at tage din online tilstedeværelse til det
+            næste niveau. Kontakt mig i dag for at starte din rejse mod en bedre
+            online oplevelse for dig og dine besøgende.
           </>
         )}
       />
+      <ProfessionalProjects />
+
       <PersonalProjects />
     </Layout>
   );
