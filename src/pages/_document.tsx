030ee9f44150412716d9a2befbcd4082f7e44bb3
chrene <chrene91@gmail.com>
2023-03-14 22:07:14 +0100

    Revert "figpii in body"
    This reverts commit 5424cde083c8e20bbbbf7233b7ca71f7507ac2da.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html lang="da">
       <Head />
+      <script
+        id="piiTester"
+        type="text/javascript"
+        async
+        crossOrigin="anonymous"
+        src="//tracking-cdn.figpii.com/72d0a38a1537770a21c58808e4eadb23.js"
+      ></script>
       <body>
-        <script
-          id="piiTester"
-          type="text/javascript"
-          async
-          crossOrigin="anonymous"
-          src="//tracking-cdn.figpii.com/72d0a38a1537770a21c58808e4eadb23.js"
-        ></script>
         <Main />
         <NextScript />
       </body>
